157c2f73a203416a41a6949c25948d45c8bd5c3e
AMAN YADAV <AMAN YADAV@LAPTOP-SKA4PUMJ>
2024-07-26 10:52:31 +0530

    changes

--- Gitthub/src/Simple_Project/Main.java
+++ Gitthub/src/Simple_Project/Main.java
@@ -2,8 +2,9 @@ package Simple_Project;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("gitgub Project");
+		System.out.println("gitgub");
 		System.out.println("2 project");
+		System.out.println("Project main");
 	}
 
 }
